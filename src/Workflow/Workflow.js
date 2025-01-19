f33f146f2a8981fd72b54075ba9cfb290eaff00a
ANKIT KUMAR MISHRA <ankit.mishra@cloudkaptan.com>
2025-01-19 19:21:38 +0530

    feat:(main) edge svg animations

--- src/Workflow/Workflow.js
+++ src/Workflow/Workflow.js
@@ -1,4 +1,4 @@
-import { ReactFlow,Background,Controls, useNodesState,useEdgesState, BackgroundVariant, ConnectionMode } from '@xyflow/react';
+import { ReactFlow,Background,Controls, useNodesState,useEdgesState, BackgroundVariant, ConnectionMode, MarkerType } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { useCallback } from 'react';
 import { addEdge } from '@xyflow/react';
@@ -6,10 +6,14 @@ import {v4 as uuid} from "uuid"
 import { COMPONENTS,initialNodes,initialEdges } from '../constants';
 import { Box } from '@mui/material';
 import ElectricalComponent from '../Components/ElectricalComponent';
+import Wire from '../Components/Wire';
 
 
 const nodeTypes ={
   electricalComponent : ElectricalComponent
+}
+const edgeTypes = {
+  wire:Wire
 }
 
 export const Workflow = () => {
@@ -18,7 +22,14 @@ export const Workflow = () => {
 
   const onConnect = useCallback((connection) => {
     const edge = {...connection,
+      type:"wire",
         id:uuid(),
+        markerEnd:{
+          type:MarkerType.ArrowClosed,
+          width:20,
+          height:20,
+          color:"#FFC300",
+        }
         };
         setEdges((eds)=> eds.concat(edge));
   },[])
@@ -31,10 +42,20 @@ export const Workflow = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         connectionMode={ConnectionMode.Loose}
-        nodeTypes={nodeTypes}>
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        onConnect={onConnect}>
         <Background variant={BackgroundVariant.Lines} gap={10} color="#f1f1f1" id="1"/>
         <Background variant={BackgroundVariant.Lines} gap={100} color="#ccc" id="2"/>
         <Controls />
+        <svg>
+          <defs>
+            <linearGradient id="wire">
+                <stop offset="0%" stopColor='#ecff02'/>
+                <stop offset="100%" stopColor='#f69900'/>
+            </linearGradient>
+          </defs>
+        </svg>
       </ReactFlow>
     </Box>
       
